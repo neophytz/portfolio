207e06f2328d2a257d983d21da239c98c35b1af9
NIKHIL GUPTA <nikhilgupta8800@gmail.com>
2022-11-04 23:37:43 +0530

    project changes made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Hero } from "./components/Hero";
 import Navbar from "./components/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import About from "./components/About";
+import Skill from "./components/Skill";
+
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <>
     <Navbar/>
     <Hero/>
+    <About/>
+    <Skill/>
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export const Hero = () => {
+  const designation = "Designer";
+ 
   return (
-    <div>hero</div>
+    <section id="hero" className="d-flex flex-column justify-content-center">
+    <div className="container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Brandon Johnson</h1>
+      <p>I'm {designation}</p>
+      <div className="social-links">
+        <a href="#!" className="twitter"><i className="fa-brands fa-twitter"></i></a>
+        <a href="#!" className="facebook"><i className="fa-brands fa-facebook"></i></a>
+        <a href="#!" className="instagram"><i className="fa-brands fa-instagram"></i></a>
+        <a href="#!" className="google-plus"><i className="fa-brands fa-skype"></i></a>
+        <a href="#!" className="linkedin"><i className="fa-brands fa-linkedin"></i></a>
+      </div>
+    </div>
+  </section>
   )
 }
